9caf5338f71c0a07f59406924085e310dcd1dd94
YuraBilyk <118901096+YuraBilyk@users.noreply.github.com>
2022-12-21 22:24:53 +1000

    Updatee

--- src/js/script.js
+++ src/js/script.js
@@ -1,28 +1,79 @@
-var pictures = document.getElementById('pictures')
 
-var elem = document.createElement("img")
 
 var Everybody = new Array(
-    '../img/work/work-1.png',
-    'work-2.png',
-    'work-3.png',
-    'work-4.png',
-    'work-5.png',
-    'work-6.png',
-    'work-7.png',
-    'work-8.png',
-    'work-9.png',
+    'src/img/work/work-1.png',
+    'src/img/work/work-2.png',
+    'src/img/work/work-3.png',
+    'src/img/work/work-4.png',
+    'src/img/work/work-5.png',
+    'src/img/work/work-6.png',
+    'src/img/work/work-7.png',
+    'src/img/work/work-8.png',
+    'src/img/work/work-9.png',
     );
-elem.src = Everybody[1];
     
+var Site = new Array(
+    'src/img/work/work-2.png',
+    'src/img/work/work-3.png',
+    'src/img/work/work-5.png',
+    'src/img/work/work-9.png',
+    );
+
+var Application = new Array(
+    'src/img/work/work-1.png',
+    'src/img/work/work-4.png',
+    'src/img/work/work-8.png',
+    );
+
+var Bots = new Array(
+    'src/img/work/work-6.png',
+    'src/img/work/work-7.png',
+    );
+
 function OneButton(){
-    document.pictures.append(elem);
+    var picture = document.getElementById('pictures')
+    picture.innerHTML = '';
 
+   for(i=0; i<Everybody.length; i++){
+        var elem = document.createElement("img")
+        elem.src = Everybody[i];
+        elem.className = 'section__pictures__img'
+        picture.appendChild(elem);
+   }
+}
 
-    //document.getElementById('pictures').src=pics[cur];
-   // for(i=1; i<Everybody.length; i++){
-        //img.innerHTML = `<img src="${Everybody[i]}">`
-    //img.innerHTML = '<img src="src/img/work/work-1.png">'
-    //pictures.insertBefore(img, 0)
-    //}
+function TwoButton(){
+    var picture = document.getElementById('pictures')
+    picture.innerHTML = '';
+
+   for(i=0; i<Site.length; i++){
+        var elem = document.createElement("img")
+        elem.src = Site[i];
+        elem.className = 'section__pictures__img'
+        picture.appendChild(elem);
+   }
+}
+
+function ThreeButton(){
+    var picture = document.getElementById('pictures')
+    picture.innerHTML = '';
+
+   for(i=0; i<Application.length; i++){
+        var elem = document.createElement("img")
+        elem.src = Application[i];
+        elem.className = 'section__pictures__img'
+        picture.appendChild(elem);
+   }
+}
+
+function FourButton(){
+    var picture = document.getElementById('pictures')
+    picture.innerHTML = '';
+
+   for(i=0; i<Bots.length; i++){
+        var elem = document.createElement("img")
+        elem.src = Bots[i];
+        elem.className = 'section__pictures__img'
+        picture.appendChild(elem);
+   }
 }
